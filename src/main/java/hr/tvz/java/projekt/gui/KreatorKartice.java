04e16ad9d977333c789ce197e9991d3fa3f4215d
MrFr4nn <franstanic132@gmail.com>
2026-06-25 22:52:35 +0200

    Promjena u fileu KreatorKartice

--- src/main/java/hr/tvz/java/projekt/gui/KreatorKartice.java
+++ src/main/java/hr/tvz/java/projekt/gui/KreatorKartice.java
@@ -6,7 +6,6 @@ import hr.tvz.java.projekt.model.RadnickaKlasa;
 import hr.tvz.java.projekt.model.SrednjaKlasa;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Border;
@@ -17,7 +16,6 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.SVGPath;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -26,35 +24,32 @@ import javafx.scene.control.ProgressBar;
 
 public class KreatorKartice {
 
-    private static final String BOJA_RADNICKA = "#8C3A36";
-    private static final String BOJA_SREDNJA = "#A6862C";
-    private static final String BOJA_KAPITALISTICKA = "#2B4C70";
-    private static final String BOJA_VLADA = "#4A4458";
-
     public VBox napraviZaglavljeKartice(KlasaIgraca igrac) {
-        String bojaTraka = odrediBojuTrake(igrac);
+        String bojaTraka = StilGumba.dohvatiBojuKlase(igrac);
 
         VBox zaglavlje = new VBox(4);
         zaglavlje.setAlignment(Pos.CENTER_LEFT);
         zaglavlje.setPadding(new Insets(14, 18, 14, 18));
-        zaglavlje.setBackground(new Background(new BackgroundFill(Color.web(bojaTraka),
-                new CornerRadii(12, 12, 0, 0, false), Insets.EMPTY)));
+        zaglavlje.setBackground(new Background(new BackgroundFill(Color.web(StilGumba.POZADINA_TAMNA),
+                new CornerRadii(6, 6, 0, 0, false), Insets.EMPTY)));
+        zaglavlje.setBorder(new Border(new BorderStroke(Color.web(bojaTraka), BorderStrokeStyle.SOLID,
+                new CornerRadii(0), new BorderWidths(0, 0, 2, 0))));
 
         HBox redIkoneINaziva = new HBox(10);
         redIkoneINaziva.setAlignment(Pos.CENTER_LEFT);
 
         SVGPath ikona = napraviIkonuKlase(igrac);
-        ikona.setFill(Color.WHITE);
+        ikona.setFill(Color.web(bojaTraka));
 
-        Label oznakaUloge = new Label(odrediNazivUloge(igrac));
-        oznakaUloge.setFont(Font.font("Georgia", FontWeight.BOLD, 17));
-        oznakaUloge.setTextFill(Color.WHITE);
+        Label oznakaUloge = new Label(odrediNazivUloge(igrac).toUpperCase());
+        oznakaUloge.setFont(Font.font("Arial Black", FontWeight.BOLD, 16));
+        oznakaUloge.setTextFill(Color.web(bojaTraka));
 
         redIkoneINaziva.getChildren().addAll(ikona, oznakaUloge);
 
         Label oznakaIgraca = new Label(igrac.getNaziv());
         oznakaIgraca.setFont(Font.font("Verdana", 11));
-        oznakaIgraca.setTextFill(Color.web("#F0F0F0"));
+        oznakaIgraca.setTextFill(Color.web(StilGumba.TEKST_SIVI));
 
         zaglavlje.getChildren().addAll(redIkoneINaziva, oznakaIgraca);
         return zaglavlje;
@@ -64,36 +59,37 @@ public class KreatorKartice {
         kartica.setPadding(new Insets(0, 0, 18, 0));
         kartica.setAlignment(Pos.TOP_LEFT);
         kartica.setPrefWidth(260);
-        kartica.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(12), Insets.EMPTY)));
+        kartica.setBackground(new Background(new BackgroundFill(Color.web(StilGumba.POVRSINA_TAMNA), new CornerRadii(6), Insets.EMPTY)));
         if (naPotezu) {
-            kartica.setBorder(new Border(new BorderStroke(Color.web("#C9A227"), BorderStrokeStyle.SOLID,
-                    new CornerRadii(12), new BorderWidths(3))));
+            kartica.setBorder(new Border(new BorderStroke(Color.web("#00D4FF"), BorderStrokeStyle.SOLID,
+                    new CornerRadii(6), new BorderWidths(3))));
+            kartica.setEffect(StilGumba.napraviNeonSjenu("#00D4FF"));
         } else {
-            kartica.setBorder(new Border(new BorderStroke(Color.web("#D8CDB8"), BorderStrokeStyle.SOLID,
-                    new CornerRadii(12), new BorderWidths(1.5))));
+            kartica.setBorder(new Border(new BorderStroke(Color.web(StilGumba.TEKST_SIVI), BorderStrokeStyle.SOLID,
+                    new CornerRadii(6), new BorderWidths(1))));
+            kartica.setEffect(null);
         }
-        kartica.setEffect(napraviSjenu());
     }
 
     public Label napraviOznaku(String tekst) {
         Label oznaka = new Label(tekst);
-        oznaka.setTextFill(Color.web("#3A332B"));
+        oznaka.setTextFill(Color.web(StilGumba.TEKST_SVIJETLI));
         oznaka.setFont(Font.font("Verdana", 13));
         return oznaka;
     }
 
     public Label napraviOznakuBodova(KlasaIgraca igrac) {
         Label oznakaBodova = new Label("BODOVI: " + igrac.getBodoviPobjede());
-        oznakaBodova.setFont(Font.font("Verdana", FontWeight.BOLD, 14));
-        oznakaBodova.setTextFill(Color.web(odrediBojuTrake(igrac)));
+        oznakaBodova.setFont(Font.font("Arial Black", FontWeight.BOLD, 15));
+        oznakaBodova.setTextFill(Color.web(StilGumba.dohvatiBojuKlase(igrac)));
         return oznakaBodova;
     }
 
     public VBox napraviMiniGraf(KlasaIgraca igrac, double trenutnaVrijednost, double maksimum, String oznakaGrafa) {
         VBox blok = new VBox(4);
-        Label naslovGrafa = new Label(oznakaGrafa);
-        naslovGrafa.setFont(Font.font("Verdana", 10));
-        naslovGrafa.setTextFill(Color.web("#6B6357"));
+        Label naslovGrafa = new Label(oznakaGrafa.toUpperCase());
+        naslovGrafa.setFont(Font.font("Verdana", FontWeight.BOLD, 9));
+        naslovGrafa.setTextFill(Color.web(StilGumba.TEKST_SIVI));
 
         ProgressBar trakaNapretka = new ProgressBar();
         double udio = trenutnaVrijednost / maksimum;
@@ -105,18 +101,11 @@ public class KreatorKartice {
         }
         trakaNapretka.setProgress(udio);
         trakaNapretka.setPrefWidth(220);
-        trakaNapretka.setStyle("-fx-accent: " + odrediBojuTrake(igrac) + ";");
+        String bojaTrake = StilGumba.dohvatiBojuKlase(igrac);
+        trakaNapretka.setStyle("-fx-accent: " + bojaTrake + "; -fx-control-inner-background: #0D0D12;");
 
         blok.getChildren().addAll(naslovGrafa, trakaNapretka);
         return blok;
-    }
-
-    private DropShadow napraviSjenu() {
-        DropShadow sjena = new DropShadow();
-        sjena.setRadius(12);
-        sjena.setOffsetY(5);
-        sjena.setColor(Color.color(0, 0, 0, 0.3));
-        return sjena;
     }
 
     private String odrediNazivUloge(KlasaIgraca igrac) {
@@ -131,18 +120,6 @@ public class KreatorKartice {
         }
     }
 
-    private String odrediBojuTrake(KlasaIgraca igrac) {
-        if (igrac instanceof RadnickaKlasa) {
-            return BOJA_RADNICKA;
-        } else if (igrac instanceof SrednjaKlasa) {
-            return BOJA_SREDNJA;
-        } else if (igrac instanceof KapitalistickaKlasa) {
-            return BOJA_KAPITALISTICKA;
-        } else {
-            return BOJA_VLADA;
-        }
-    }
-
     private SVGPath napraviIkonuKlase(KlasaIgraca igrac) {
         SVGPath ikona = new SVGPath();
         ikona.setScaleX(0.9);
@@ -153,9 +130,10 @@ public class KreatorKartice {
         } else if (igrac instanceof SrednjaKlasa) {
             ikona.setContent("M4 8 H20 V18 H4 Z M9 8 V5 H15 V8");
         } else if (igrac instanceof KapitalistickaKlasa) {
-            ikona.setContent("M4 10 H20 V18 H4 Z M4 10 L12 4 L20 10 M8 13 H10 V16 H8 Z M14 13 H16 V16 H14 Z");
+            ikona.setContent("M3 18 H21 V20 H3 Z M5 18 V10 H7 V18 Z M9 18 V6 H11 V18 Z M13 18 V11 H15 V18 Z M17 18 V8 H19 V18 Z");
         } else {
-            ikona.setContent("M4 20 V10 L12 4 L20 10 V20 M9 20 V14 H15 V20 M6 10 V20 M18 10 V20");
+            ikona.setContent("M4 20 H20 V21 H4 Z M5 20 V11 H6.5 V20 Z M8 20 V11 H9.5 V20 Z M11.25 20 V11 H12.75 V20 Z "
+                    + "M14.5 20 V11 H16 V20 Z M17.5 20 V11 H19 V20 Z M3 11 L12 4 L21 11 Z");
         }
         return ikona;
     }
