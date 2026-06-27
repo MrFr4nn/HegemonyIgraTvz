406d3b12c14d11c0786eac0decf24b21aec53a4d
MrFr4nn <franstanic132@gmail.com>
2026-06-27 13:55:46 +0200

    Popravljen bug minimiziranja prozora

--- src/main/java/hr/tvz/java/projekt/PokretacAplikacije.java
+++ src/main/java/hr/tvz/java/projekt/PokretacAplikacije.java
@@ -3,15 +3,20 @@ package hr.tvz.java.projekt;
 import hr.tvz.java.projekt.gui.GlavniProzor;
 import hr.tvz.java.projekt.gui.PocetniEkran;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 
 public class PokretacAplikacije extends Application {
 
     @Override
     public void start(Stage glavnaScena) {
+        glavnaScena.setResizable(true);
         PocetniEkran pocetniEkran = new PocetniEkran(glavnaScena, listaIgraca -> {
-            GlavniProzor glavniProzor = new GlavniProzor(glavnaScena, listaIgraca);
-            glavniProzor.prikaziProzor();
+            Platform.runLater(() -> {
+                GlavniProzor glavniProzor = new GlavniProzor(glavnaScena, listaIgraca);
+                glavniProzor.prikaziProzor();
+                glavnaScena.setIconified(false);
+            });
         });
         pocetniEkran.prikaziEkran();
     }
