5697f80a0a2e023ba163c55415e79a5434dac15a
MrFr4nn <franstanic132@gmail.com>
2026-07-06 18:39:53 +0200

    Popravljanje pokretaca sa sonar qube

--- src/main/java/hr/tvz/java/projekt/PokretacAplikacije.java
+++ src/main/java/hr/tvz/java/projekt/PokretacAplikacije.java
@@ -2,25 +2,34 @@ package hr.tvz.java.projekt;
 
 import hr.tvz.java.projekt.gui.GlavniProzor;
 import hr.tvz.java.projekt.gui.PocetniEkran;
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import java.util.logging.Logger;
 
 public class PokretacAplikacije extends Application {
 
+    private static final Logger LOG = Logger.getLogger(PokretacAplikacije.class.getName());
+
     @Override
     public void start(Stage glavnaScena) {
+        glavnaScena.setResizable(true);
         PocetniEkran pocetniEkran = new PocetniEkran(glavnaScena, listaIgraca -> {
-            glavnaScena.hide();
-            Stage noviProzor = new Stage();
-            GlavniProzor glavniProzor = new GlavniProzor(noviProzor, listaIgraca);
-            glavniProzor.prikaziProzor();
-            noviProzor.setMaximized(true);
+            PauseTransition odgoda = new PauseTransition(Duration.millis(50));
+            odgoda.setOnFinished(dogadjaj -> {
+                GlavniProzor glavniProzor = new GlavniProzor(glavnaScena, listaIgraca);
+                glavniProzor.prikaziProzor();
+                glavnaScena.setIconified(false);
+                glavnaScena.setMaximized(true);
+            });
+            odgoda.play();
         });
         pocetniEkran.prikaziEkran();
     }
 
     public static void main(String[] argumenti) {
-        System.err.println("Pokretanje...");
+        LOG.info("Pokretanje aplikacije Hegemony: Lead Your Class to Victory...");
         launch(argumenti);
     }
 }
